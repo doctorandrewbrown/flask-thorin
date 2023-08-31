53aaf10c01a2ccd3397da8c434f87a2006d79683
Andy Brown <andrewdavidbrown@zohomail.eu>
2023-08-31 13:45:15 +0100

    add import os

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 
@@ -26,4 +27,7 @@ def post():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
